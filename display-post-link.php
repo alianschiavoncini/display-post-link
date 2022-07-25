99ca40a04c4eca39bcb4151e43482274bbf62b75
Alian Schiavoncini <alian@alian.it>
2022-07-25 18:02:10 +0200

    Version 1.0.0.

---
+++ display-post-link.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Plugin Name: Display Post Link
+ * Plugin URI: https://wordpress.org/plugins/display-post-link
+ * Description: Displays the post link via shortcode.
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Author: Alian Schiavoncini
+ * Author URI: https://www.alian.it
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI: https://wordpress.org/plugins/display-post-link
+ * Text Domain: display-post-link
+ */
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+// Main Display Post Link function
+function display_post_link($attr) {
+
+	$args = shortcode_atts( array(
+		'id' 			=> $attr['id'],
+		'url' 			=> false,
+		'title' 		=> false,
+		'custom_title' 	=> false,
+	), $attr );
+
+	if ( is_numeric($args['id']) ) {
+
+		$args['url'] = get_permalink( $args['id'] );
+		$args['title'] = get_the_title( $args['id'] );
+
+	}else{
+
+		switch ($args['id']) {
+			// WooCommerce
+			// woocommerce_shop_page_id
+			case 'woocommerce-shop' :
+				if (get_option('woocommerce_shop_page_id') > 0) {
+					$args['url'] = get_permalink( get_option('woocommerce_shop_page_id') );
+					$args['title'] = get_the_title( get_option('woocommerce_shop_page_id') );
+				}
+				break;
+
+			case 'woocommerce-terms' :
+				if ( class_exists( 'woocommerce' ) && ( wc_get_page_id( 'terms' ) > 0) ) {
+					$args['url'] = get_permalink( wc_get_page_id( 'terms' ) );
+					$args['title'] = get_the_title( wc_get_page_id( 'terms' ) );
+				}
+				break;
+
+			case 'woocommerce-cart' :
+				if ( class_exists( 'woocommerce' ) && ( wc_get_page_id( 'cart' ) > 0) ) {
+					$args['url'] = get_permalink( wc_get_page_id( 'cart' ) );
+					$args['title'] = get_the_title( wc_get_page_id( 'cart' ) );
+				}
+				break;
+
+			case 'woocommerce-myaccount' :
+				if ( class_exists( 'woocommerce' ) && ( wc_get_page_id( 'myaccount' ) > 0) ) {
+					$args['url'] = get_permalink( wc_get_page_id( 'myaccount' ) );
+					$args['title'] = get_the_title( wc_get_page_id( 'myaccount' ) );
+				}
+				break;
+
+			case 'woocommerce-checkout' :
+				if ( class_exists( 'woocommerce' ) && ( wc_get_page_id( 'checkout' ) > 0) ) {
+					$args['url'] = get_permalink( wc_get_page_id( 'checkout' ) );
+					$args['title'] = get_the_title( wc_get_page_id( 'checkout' ) );
+				}
+				break;
+				break;
+
+			// WP Options
+			// wp_page_for_privacy_policy
+			case 'privacy-policy' :
+			case 'privacypolicy' :
+			case 'privacy' :
+				if (get_option('wp_page_for_privacy_policy') > 0) {
+					$args['url'] = get_permalink( get_option('wp_page_for_privacy_policy') );
+					$args['title'] = get_the_title( get_option('wp_page_for_privacy_policy') );
+				}
+				break;
+
+			// page_for_posts
+			case 'blog' :
+			case 'postspage' :
+				if (get_option('page_for_posts') > 0) {
+					$args['url'] = get_permalink( get_option('page_for_posts') );
+					$args['title'] = get_the_title( get_option('page_for_posts') );
+				}
+				break;
+
+			// page_on_front
+			case 'homepage' :
+				if (get_option('page_on_front') > 0) {
+					$args['url'] = get_permalink( get_option('page_on_front') );
+					$args['title'] = get_the_title( get_option('page_on_front') );
+				}
+				break;
+		}
+
+	}
+
+	if (!empty($args['custom_title'])) {
+		$args['title'] = $args['custom_title'];
+	}
+
+	$str = false;
+	if (!empty($args['url']) && !empty($args['title'])) {
+		$str = sprintf( '<a class="display-post-link" href="%s">%s</a>', esc_url($args['url']), esc_html($args['title']) );
+	}
+
+	return $str;
+}
+add_shortcode( 'display-post-link' , 'display_post_link' );
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Display Post Link ===
+Contributors: alian
+Tags: display link, show link, blog link, privacy policy link, WooCommerce pages link
+Requires at least: 3.0.1
+Tested up to: 5.8.1
+Stable tag:	3.2.3
+Requires PHP: 5.2.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Display post link via shortcode in post content or widget area. It is useful for the home page, blog page, privacy policy page and WooCommerce pages as there is no need to update the links manually. Compatible with the WPML plugin.
+
+== Description ==
+The shortcode [display-post-link] must be used to display the link in combination with the ID parameter (required).
+
+The WordPress options IDs available are: homepage, blog, privacy-policy.
+Example [display-post-link id="privacy-policy"]
+
+For WooCoommerce plugin, you can use these options IDs: woocommerce-shop, woocommerce-terms, woocommerce-cart, woocommerce-myaccount, woocommerce-checkout. WooCommerce plugin must be installed and active.
+
+You can also use the post ID number, but if you delete the post, the shortcode will not be able to display the link. For this reason, it is recommended to use the ID in number format only if you are sure you will not trash/delete the post or create a new one.
+
+The custom_title parameter can also be used to display a custom text link instead the post title.
+Example: [display-post-link id="privacy-policy" custom_title="Check it out our Privacy Policy!"]
+
+== Installation ==
+1. Upload the entire `display-post-link` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the **Plugins** screen (**Plugins > Installed Plugins**).
+
+== Screenshots ==
+
+== Upgrade Notice ==
+
+= 0.1 (2022-08-01) =
+* Initial version on WP repository
